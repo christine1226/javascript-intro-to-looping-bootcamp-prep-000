0cada8e6f28a9d0023ee294fab819d7c064f9dc3
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-08-19 23:16:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,7 +11,7 @@ function forLoop(array){
   for(let i=1; i<25; i++){
     if (i===1){
       array.push("I am 1 strange loops.");
-    } else { array.push("I am ${i} strange loops.");
+    } else { array.push(`I am ${i} strange loops.`);
     }
   }
   return array;
